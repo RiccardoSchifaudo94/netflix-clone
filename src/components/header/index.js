06beb897751ee403b0c220e9c2d755b8c2631cc7
Ricky <mr.emps94@gmail.com>
2021-01-31 21:32:38 +0100

    create header profile section

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Background , Container, Logo, ButtonLink } from './styles/header';
+import { 
+        Background , 
+        Container, 
+        Logo, 
+        ButtonLink , 
+        Feature, 
+        FeatureCallOut, 
+        Text, 
+        Link, 
+        Group,
+        Picture, 
+        Profile,
+        Dropdown
+} from './styles/header';
 
 export default function Header({bg=true, children,...restProps}) {
         return bg ? <Background {...restProps}>{children}</Background> : children;
 }
 
+Header.Feature = function HeaderFeature({children, ...restProps}){
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({children, ...restProps}){
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+
 Header.Frame = function HeaderFrame({children,...restProps}){
     return <Container {...restProps}>{children}</Container>
+}
+Header.Group = function HeaderGroup({children,...restProps}){
+    return <Group {...restProps}>{children}</Group>
+}
+
+Header.Text = function HeaderText({children, ...restProps}){
+    return <Text {...restProps}>{children}</Text>
+}
+
+Header.TextLink = function HeaderTextLink({children, ...restProps}){
+    return <Link {...restProps}>{children}</Link>
 }
 
 Header.ButtonLink = function HeaderButtonLink({children,...restProps}){
     return <ButtonLink {...restProps}>{children}</ButtonLink>
 }
 
+Header.Profile = function HeaderProfile({children,...restProps}){
+    return <Profile {...restProps}>{children}</Profile>;
+}
+Header.Dropdown = function HeaderDropdown({children,...restProps}){
+    return <Dropdown {...restProps}>{children}</Dropdown>;
+}
+Header.Picture = function HeaderPicture({src,...restProps}){
+    return <Picture {...restProps} src={`/images/users/${src}.png`}/>
+}
 Header.Logo = function HeaderLogo({to, ...restProps}){
     return (
         <ReactRouterLink to={to}>
